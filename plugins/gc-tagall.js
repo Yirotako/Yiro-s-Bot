57b89e77384d2b361c3142a94e6acb93282b5f74
LxShadow <152157350+LxShadow@users.noreply.github.com>
2024-07-17 20:28:41 -0600

    𝙔𝙄𝙍𝙊𝙎 𝘽𝙊𝙏

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,12 +5,12 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕 ${pesan}`;
+  const oi = `𝙔𝙄𝙍𝙊𝙎 𝘽𝙊𝙏 ${pesan}`;
   let teks = `𝑨𝒄𝒕𝒊𝒗𝒆𝒏𝒔𝒆 𝑨𝒍𝒗!!\n\n ${oi}\n\n 𝑴𝒆𝒏𝒄𝒊𝒐𝒏𝒆𝒔:\n\n`;
   for (const mem of participants) {
-    teks += `❄ @${mem.id.split('@')[0]}\n`;
+    teks += `🍆 @${mem.id.split('@')[0]}\n`;
   }
-  teks += `𝑺𝒉𝒂𝒅𝒐𝒘𝒔 𝑩𝒐𝒕`;
+  teks += `𝙔𝙄𝙍𝙊𝙎 𝘽𝙊𝙏`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
